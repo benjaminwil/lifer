1c4c3c7591ebd1e95d1a1ee87b65af2150920d27
= <hey@benjaminwil.info>
2022-09-06 17:48:42 -0600

    Tweak RSpec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,15 +38,21 @@ module Lifer::FileHelpers
 end
 
 RSpec.configure do |config|
+  # Use the built-in `documentation` formatter for runs.
+  config.add_formatter :documentation
+
   config.after(:each) do
     lose_support_config
   end
 
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  # If RSpec runs on somethign other than TTY, try to display colours.
+  config.color_mode = :on
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
