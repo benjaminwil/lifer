7a95055e19ec72fba6799c8606b7882981a6252f
= <hey@benjaminwil.info>
2024-10-29 19:58:24 -0700

    Handle common content types via the dev server
    I don't feel like it's worth testing this. Buuut it would be nice to
    test and not manage this list manually in the future.

--- lib/lifer/dev/response.rb
+++ lib/lifer/dev/response.rb
@@ -18,12 +18,19 @@ module Lifer::Dev
       [File.read(path)]
     end
 
+    # FIXME:
+    # It would be very nice to not manually manage this list of content types.
+    # Is there a nice, dependency-free way to do this?
+    #
     def content_type
       case File.extname(path)
-      when ".html" then "text/html"
       when ".css" then "text/css"
-      when ".js" then "text/javascript"
+      when ".html" then "text/html"
       when ".ico" then "image/ico"
+      when ".js" then "text/javascript"
+      when ".map" then "application/json"
+      when ".woff" then "application/font-woff2"
+      when ".woff2" then "application/font-woff2"
       else
         raise NotImplementedError,
           I18n.t("dev.router.content_type_not_implemented", path:)
