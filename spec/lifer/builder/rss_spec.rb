3ccce44c88e80348d0d426d90d354e4e85e5cd1c
= <hey@benjaminwil.info>
2025-02-23 14:40:30 -0800

    Provide additional RSS feed configuration points
    It is reasonable to want more fine-grained control over your RSS feed
    contents. This commit provides a lot of additional functionality for RSS
    feed generation. See the diff's documentation and changes for more
    information.

--- spec/lifer/builder/rss_spec.rb
+++ spec/lifer/builder/rss_spec.rb
@@ -16,69 +16,87 @@ RSpec.describe Lifer::Builder::RSS do
       MARKDOWN
     }
   }
-  let(:config) {
-    <<~CONFIG
-      rss: my-feed.xml
-      subdirectory_one:
-        blah: blah
-    CONFIG
-  }
 
   describe ".execute" do
     subject { described_class.execute root: project.brain.root }
 
-    it "generates a single RSS feed" do
-      expect { subject }
-        .to change {
-          Dir.glob("#{project.brain.output_directory}/**/my-feed.xml").count
-        }
-        .from(0)
-        .to(1)
+    context "with a boolean RSS configuration" do
+      let(:config) { "rss: true" }
+
+      it "generates a single a single RSS feed to the collection's name + `.xml`" do
+        expect { subject }
+          .to change {
+            Dir.glob("#{project.brain.output_directory}/**/root.xml").count
+          }
+          .from(0)
+          .to(1)
+      end
     end
 
-    it "generates all feed items" do
-      # We're excluding the collection `subdirectory_one` from our results, though.
-      #
-      article_count = Dir.glob("#{project.brain.root}/*.md").count
+    context "with a simple RSS configuration (filename only)" do
+      let(:config) {
+        <<~CONFIG
+          rss: my-feed.xml
+          subdirectory_one:
+            blah: blah
+        CONFIG
+      }
 
-      subject
+      it "generates a single RSS feed" do
+        expect { subject }
+          .to change {
+            Dir.glob("#{project.brain.output_directory}/**/my-feed.xml").count
+          }
+          .from(0)
+          .to(1)
+      end
 
-      generated_feed = generated_feed_document "my-feed.xml"
-      feed_items = generated_feed.xpath "//item"
+      it "generates the correct amount of feed items" do
+        # We're excluding the entries in `subdirectory_one` here.
+        #
+        article_count = Dir.glob("#{project.brain.root}/*.md").count
 
-      expect(feed_items.count).to eq article_count
+        subject
+
+        feed_items = generated_feed_document("my-feed.xml").xpath "//item"
+
+        expect(feed_items.count).to eq article_count
+      end
+
+      it "generates parseable article metadata correctly" do
+        subject
+
+        generated_feed = generated_feed_document "my-feed.xml"
+
+        managing_editor = generated_feed.xpath "//managingEditor"
+        expect(managing_editor.text).to eq "Admin" # Default author.
+
+        entry = generated_feed.xpath("//item").css("link")
+          .detect { _1.text == "https://example.com/tiny_entry.html" }
+          .parent
+        expect(text_from entry, :title).to eq "Untitled Entry"
+        expect(text_from entry, :description).to eq "A testable entry."
+        expect(text_from entry, :content).to fuzzy_match <<~CONTENT
+          <h1 id="tiny">Tiny</h1>
+          <p>A testable entry.</p>
+        CONTENT
+
+        expect { DateTime.parse text_from(entry, :pubDate) }.not_to raise_error
+        expect { DateTime.parse text_from(entry, :date) }.not_to raise_error
+      end
+
+      it "properly escapes HTML nodes in the article contents" do
+        subject
+
+        feed_contents = File.read(
+          Dir.glob("#{project.brain.output_directory}/**/my-feed.xml").first
+         )
+
+        expect { RSS::Parser.parse feed_contents }.not_to raise_error
+      end
     end
 
-    it "generates parseable article metadata correctly" do
-      subject
-
-      generated_feed = generated_feed_document "my-feed.xml"
-      entry = generated_feed.xpath("//item").css("link")
-        .detect { _1.text == "https://example.com/tiny_entry.html" }
-        .parent
-
-      expect(text_from entry, :title).to eq "Untitled Entry"
-      expect(text_from entry, :description).to eq "A testable entry."
-      expect(text_from entry, :content).to fuzzy_match <<~CONTENT
-        <h1 id="tiny">Tiny</h1>
-        <p>A testable entry.</p>
-      CONTENT
-
-      expect { DateTime.parse text_from(entry, :pubDate) }.not_to raise_error
-      expect { DateTime.parse text_from(entry, :date) }.not_to raise_error
-    end
-
-    it "properly escapes HTML nodes in the article contents", :aggregate_failures do
-      subject
-
-      feed_contents = File.read(
-        Dir.glob("#{project.brain.output_directory}/**/my-feed.xml").first
-      )
-
-      expect { RSS::Parser.parse feed_contents }.not_to raise_error
-    end
-
-    context "when many collections are configured" do
+    context "when many collections are configured (filename only)" do
       let(:files) {
         {"root-entry.md" => nil, "subdirectory_one/entry.md" => nil}
       }
@@ -99,6 +117,44 @@ RSpec.describe Lifer::Builder::RSS do
         #
         expect(Dir.glob(pattern).map { File.basename _1 })
           .to contain_exactly "default.xml", "subdirectory-one.xml"
+      end
+    end
+
+    context "when a single collection is configured with all options" do
+      let(:config) {
+        <<~CONFIG
+          rss:
+            count: 1
+            managing_editor: custom@example.com (Custom Editor)
+            url: custom.xml
+        CONFIG
+      }
+      let(:files) { {"one.md" => "One", "two.md" => "Two"} }
+
+      it "obeys URL configuration" do
+        expect { subject }
+          .to change { generated_feed_document("custom.xml") }
+          .from(nil)
+          .to(instance_of Nokogiri::XML::Document)
+      end
+
+      it "obeys count configuration" do
+        subject
+
+        feed_items = generated_feed_document.xpath "//item"
+
+        entry_count = files.keys.count
+
+        expect(feed_items.count).not_to eq entry_count
+        expect(feed_items.count).to eq 1
+      end
+
+      it "obeys managing editor configuration" do
+        subject
+
+        managing_editor = generated_feed_document.xpath "//managingEditor"
+
+        expect(managing_editor.text).to eq "custom@example.com (Custom Editor)"
       end
     end
   end
